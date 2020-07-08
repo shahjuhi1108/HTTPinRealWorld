86e53383afc3f2914a4e64861f962032fa5b3541
Juhi <shahjuhi1108@gmail.com>
2020-07-07 23:06:46 -0700

    add threading to server

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -5,6 +5,8 @@
 import http.server
 import requests
 import os
+import threading
+from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -97,8 +99,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write(
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
+    class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+        "This is an HTTPServer that supports thread-based concurrency."
+
 if __name__ == '__main__':
     server_address = ('', int(os.environ.get('PORT', '8000')))
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
 
